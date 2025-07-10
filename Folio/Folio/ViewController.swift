ab553eba017627e464b5563b569bd95ca8db0a6e
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-07-10 19:16:19 -0300

    Initial commit for Folio iOS project
    Add base Xcode project files, including AppDelegate, SceneDelegate, ViewController, asset catalogs, storyboards, and Info.plist for the new Folio iOS application.

---
+++ Folio/Folio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Folio
+//
+//  Created by Lucas Dal Pra Brascher on 10/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
